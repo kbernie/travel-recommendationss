41f5dd81a92daab9bdc8fd15bcbb280a4ecf24f5
kbernie <kolbernie@gmail.com>
2024-05-16 11:36:07 -0400

    search functionality

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -1,14 +1,37 @@
 
 function searchFunc() {
     let keyword = document.getElementById("search").value;
-    console.log(keyword);
-    console.log("button clicked");
-    
+    let result = document.getElementById("result")
+    document.getElementById("result").style.backgroundColor = "rgba(62, 158, 153, 0.5)"
+
     fetch('travel_recommendation_api.json')
         .then(response => response.json())
         .then(data => {
-            console.log(data);
-            console.log(data.countries[1].cities[1].name)
+
+            if(keyword === "beaches" || keyword === "temples") {
+                result.innerHTML = "";
+
+                for (let city of data[keyword]) {
+                    console.log(city.name) // city names
+                    result.innerHTML += `<div class="results"><img src="${city.imageUrl}" width="100% height="250px"><br>
+                    <h3>${city.name}</h3><br>
+                    <p>${city.description}</p><br>
+                    </div>`
+                }
+            } else if(keyword === "countries") {
+                result.innerHTML = "";
+
+                for (let x of data[keyword]) {
+                    let cities = x.cities;
+                    for (let city of cities) {
+                        console.log(city.name) // city names
+                        result.innerHTML += `<div class="results"><img src="${city.imageUrl}" width="100% height="250px"><br>
+                        <h3>${city.name}</h3><br>
+                        <p>${city.description}</p><br>
+                        </div>`
+                    }
+                }
+            }
         })
         .catch(error => {
             console.error('Error:', error);
@@ -25,3 +48,4 @@ countries
 
 +variations: beach, beaches, Beach, BEACH
 */
+
